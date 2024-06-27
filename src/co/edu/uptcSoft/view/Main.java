6db70602a026c012a647101d9260c9c902de65e3
DaniGuevara09 <laura.guevara02@uptc.edu.co>
2024-06-26 22:40:03 -0500

    Creation of new UpdateOrder Class

--- src/co/edu/uptcSoft/view/Main.java
+++ src/co/edu/uptcSoft/view/Main.java
@@ -3,12 +3,15 @@ package co.edu.uptcSoft.view;
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board();
+        //Board board = new Board();
 
         SpecificOrder specificOrder = new SpecificOrder();
-        specificOrder.createWindow();
+        //specificOrder.createWindow();
 
         NewOrder newOrder = new NewOrder();
         //newOrder.createWindow();
+
+        UpdateOrder updateOrder = new UpdateOrder();
+        updateOrder.createWindow();
     }
 }
